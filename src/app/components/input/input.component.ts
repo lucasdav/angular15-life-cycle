64deb0ae0209b939262c51011ffde9b576e91f64
lucasdav <lucassouzadavanso@gmail.com>
2024-03-16 16:34:00 -0300

    adicionando novos itens a lista

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputComponent implements OnInit {
 
+  valorItem!: string;
+
   constructor() { }
 
   ngOnInit(): void { }
+  adicionarItem(){
+    console.log(this.valorItem);
+  }
 }
